d49ca0a607c9fd2d4c12048f4021d28c39e19ba7
Roman Bitarovský <bitarovskyroman@gmail.com>
2021-09-28 19:16:52 +0200

    reading pcap files by temp list file

--- main.py
+++ main.py
@@ -1,12 +1,55 @@
 
+import os
+import sys
+import collections
+import datetime
+import csv
+import requests
+from scapy.all import *
+import random
+from collections import Counter
+# pip install numpy
+# pip install pandas
+# pip install matplotlib
+# pip install requests
+
+PCAP_FILES_LIST = "zoznamVstupnychFiles.txt"
+PROTOCOLS_LIST = "protocols.txt"
+
+
+# citanie ciest k suborom z pomocneho suboru PCAP_FILES_LIST
+def useFiles():
+
+    # z relativnej cesty k suboru sa vytvori relativna cesta zacinajuca v priecinku kodu
+    path_to_pcap_file = os.path.join(os.path.dirname(__file__), PCAP_FILES_LIST)
+
+    print("PCAP_FILES_LIST is: " + path_to_pcap_file)
+
+    # vytiahnutie vsetkych pcap subor z pomocneho suboru PCAP_FILES_LIST
+    temp_file = open(path_to_pcap_file, "r")
+    pcap_files_paths = temp_file.readlines()
+    temp_file.close()
+
+    for iterator, line in enumerate(pcap_files_paths, 1):
+        print("{:03}: {}".format(iterator, line), end="")
+
+
 def main():
 
+    # odchytenie vystupu do variable
     origOutput = sys.stdout
 
     filename = useFiles()
 
+    # main loop
+    while filename != None:
+
+        print("actual file: " + filename)
+
+        filename = useFiles()
+
 
 if __name__ == '__main__':
-    print('PyCharm')
+    print('** PyCharm starting.. **')
     main()
 # end of program
